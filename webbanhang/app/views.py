ce4a48516ff8d3103b8e76b4c3684ed454286777
longnhat197 <longnhat515@gmail.com>
2026-04-03 00:10:15 +0700

    feat: implement cart and checkout pages with base template and routing

--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "app/home.html")
+
+def cart(request):
+    context = {}
+    return render(request, "app/cart.html", context)
+
+def checkout(request):
+    context = {}
+    return render(request, "app/checkout.html", context)
--- webbanhang/webbanhang/settings.py
+++ webbanhang/webbanhang/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +74,16 @@ WSGI_APPLICATION = "webbanhang.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+SUPABASE_DB_URL = "postgresql://postgres.nanoifhuxjyueqvadeeh:buitien1231@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres"
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(SUPABASE_DB_URL, conn_max_age=600),
 }
 
 
